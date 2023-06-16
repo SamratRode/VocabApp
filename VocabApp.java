c5c2618ce33b4ba7911e658054c29625e0ea295c
Samrat Rode <56781677+SamratRode@users.noreply.github.com>
2023-06-16 19:06:44 +0530

    Create VocabApp.java

---
+++ VocabApp.java
@@ -0,0 +1,263 @@
+import javax.swing.*;
+import java.util.*;
+import java.awt.FlowLayout;
+
+public class Vocab {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        ArrayList<String> words=new ArrayList<String>();
+        words.add("Malevolent");
+        words.add("Pernicious");
+        words.add("Extemporanious");
+        words.add("Forthright");
+        words.add("Outspoken");
+        words.add("Inured");
+        words.add("Mediocre");
+        words.add("Fledgling");
+        words.add("Nuanced");
+        words.add("Delineate");
+        words.add("Splendor");
+        words.add("Tranquility");
+        words.add("Milieu");
+        words.add("Deleterious");
+        words.add("Mundane");
+        words.add("Celerity");
+        words.add("Novice");
+        words.add("Conscientious");
+        words.add("Slovenliness");
+        words.add("Spite");
+        words.add("Acquiesce");
+        words.add("Austere");
+        words.add("Ambigious");
+        words.add("Relinquished");
+        words.add("Tardy");
+        words.add("Flamboyance");
+        words.add("Virtuoso");
+        words.add("Rancorous");
+        words.add("Cynical");
+        words.add("Trimmings");
+        words.add("Discard");
+        words.add("Docile");
+        words.add("Lax");
+        words.add("Impulsive");
+        words.add("Egalitarian");
+        words.add("Reverence");
+        words.add("Scrapped");
+        words.add("Connoiseur");
+        words.add("Justification");
+        words.add("Scathing");
+        words.add("Disingenuous");
+        words.add("Dauntless");
+        words.add("Dubious");
+        words.add("Deceitful");
+        words.add("Unsound");
+        words.add("Notorious");
+        words.add("Esoteric");
+        words.add("Perplexing");
+        words.add("Profound");
+        words.add("Beleagured");
+        words.add("Impediment");
+        words.add("Vigor");
+        words.add("Alacrity");
+        words.add("Embroiled");
+        words.add("Skepticism");
+        words.add("To Fathom");
+        words.add("Spurious");
+        words.add("Tempered");
+        words.add("Mandate");
+        words.add("Poignant");
+        words.add("Impeccable");
+        words.add("Immaterial");
+        words.add("Unbiased");
+        words.add("Dilemma");
+        words.add("To Embody");
+        words.add("To Curb");
+        words.add("Credible");
+        words.add("Perturbed");
+        words.add("Conducive");
+        words.add("Jerkily");
+        words.add("pusillanimous");
+        words.add("Intrepid");
+        words.add("Notions");
+        words.add("Exponential");
+        words.add("Startled");
+        words.add("To Manifest");
+        words.add("To Eschew");
+        words.add("Eccentric");
+        words.add("Coherent");
+        words.add("Erudite");
+        words.add("Detriment");
+        words.add("Empathy");
+        words.add("Embodied");
+        words.add("Splendor");
+        words.add("Sublime");
+        words.add("Dilate");
+        words.add("Esoteric");
+        words.add("Pensive");
+        words.add("Fickle");
+        words.add("Transient");
+        words.add("Endeavour");
+        words.add("Plight");
+        words.add("Transcendence");
+        words.add("Distortion");
+        words.add("Sporadic");
+        words.add("Absurd");
+        words.add("Disposition");
+        words.add("Capricious");
+        words.add("Indignant");
+        words.add("Denounce");
+        words.add("Renounce");
+        words.add("Audacious");
+        words.add("Conspicuous");
+        words.add("Allusion");
+        words.add("Animosity");
+        words.add("Extensive");
+        words.add("Epitome");
+        words.add("Iconoclastic");
+        words.add("Bloated");
+        words.add("Moribund");
+        words.add("Idolize");
+        words.add("To Cow");
+        words.add("Servitude");
+        words.add("Bleak");
+        words.add("Pervasive");
+        words.add("Pacific");
+        words.add("Entranced");
+        words.add("Enervated");
+        words.add("Lauded");
+        words.add("Reprove");
+        words.add("Disparaged");
+        words.add("Concede");
+        words.add("Poignant");
+        words.add("Histrionic");
+        words.add("Inconspicuous");
+        words.add("Protracted");
+        words.add("Papal");
+        words.add("Sinister");
+        words.add("Debauched ");
+        words.add("Antithetical");
+        words.add("Ambiguous");
+        words.add("Decry");
+        words.add("Woeful");
+        words.add("Paucity");
+        words.add("Erudite");
+        words.add("Chimera");
+        words.add("Shambolic");
+        words.add("Choleric");
+        words.add("Lateralized");
+        words.add("Valiant");
+        words.add("Inconsiderate");
+        words.add("Rekindle");
+        words.add("Inveigle");
+        words.add("Feud");
+        words.add("Conflate");
+        words.add("Extirpate");
+        words.add("Ameliorate");
+        words.add("Indefatigable");
+        words.add("Insurmountable");
+        words.add("Impeccable");
+        words.add("Surreptituous");
+        words.add("Condemn");
+        words.add("Embellishment");
+        words.add("Verbose");
+        words.add("Assiduous");
+        words.add("Succinct");
+        words.add("Excise");
+        words.add("Shackles");
+        words.add("Warranted");
+        words.add("Hidebound");
+        words.add("Emancipate");
+        words.add("Condone");
+        words.add("Reprobate");
+        words.add("Seminary");
+        words.add("Unctuous");
+        words.add("Quotidian");
+        words.add("Abstemious");
+        words.add("Conscientious");
+        words.add("Zealous");
+        words.add("Slander");
+        words.add("Conscience");
+        words.add("Crippled");
+        words.add("Bereft");
+        words.add("Imperiled");
+        words.add("Enfeebled");
+        words.add("Tepid");
+        words.add("Cunning");
+        words.add("Planted");
+        words.add("Inexorable");
+        words.add("Espouse");
+        words.add("Dissipated");
+        words.add("Euphenism");
+        words.add("Expletive");
+        words.add("Insipid");
+        words.add("Lukewarm");
+        words.add("Ingenuousness");
+        words.add("Derogatory");
+        words.add("Seizure");
+        words.add("Effaced");
+        words.add("Edification");
+        words.add("Gab");
+        words.add("Pontificate");
+        words.add("Expatiate");
+        words.add("Spurred");
+        words.add("Propelled");
+        words.add("Goaded");
+        words.add("Channelized");
+        words.add("Meander");
+        words.add("Enervated");
+        words.add("Delineate");
+        words.add("Feverish");
+        words.add("Transgression");
+        words.add("Epiphany");
+        words.add("Pussilanimous");
+        words.add("Serendipity");
+        words.add("Capricious");
+
+
+
+    
+        int min = 0; // Minimum limit
+        int max =words.size(); // Maximum limit
+        String input = JOptionPane.showInputDialog("Enter the number of lines to print:");
+        int i = Integer.parseInt(input);
+        JFrame frame = new JFrame("My Vocabulary Application");
+
+        // Set the size and close operation
+        frame.setSize(400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTextArea textArea = new JTextArea(10, 20);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        frame.getContentPane().add(scrollPane);
+
+        frame.setVisible(true);
+        while(i>0){
+            // Generate a random double between 0 (inclusive) and 1 (exclusive)
+            double randomDouble = Math.random();
+
+            // Calculate the random integer within the specified limits
+            int randomNumber = (int) (randomDouble * (max - min + 1) + min);
+
+            // System.out.println("What is the meaning of the word? " + words.get(randomNumber));
+            // sc.nextLine();
+            // Add a label to the window
+
+            
+            System.out.println("Meaning of the word "+ words.get(randomNumber));
+
+            textArea.append("Meaning of the word "+ words.get(randomNumber) + "\n");
+            
+            i--;
+
+        }
+        textArea.append("Refer the Link \n"+"https://docs.google.com/presentation/d/1jVrSqs__kJ8TJ9gSi02JXMkegS6VExMc/edit#slide=id.p10");
+
+        
+
+        
+
+
+    }
+    
+    
+}
